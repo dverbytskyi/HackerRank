d9524e4df630aee302870f83d542e2086d7472a8
Dmytro Verbytskyi <dmytro.qa.engineer@gmail.com>
2020-10-29 22:29:04 +0200

    update

--- Python Practice/The Captain's Room.py
+++ Python Practice/The Captain's Room.py
@@ -1,13 +1,14 @@
 # problem statement - https://www.hackerrank.com/challenges/py-the-captains-room/problem
 
 if __name__ == '__main__':
+
+    # doesn't pass in time limits
     size_of_each_group = int(input())
     room_number_list = list(map(int, input().split()))
-
     unique_room = [int(item) for item in set(room_number_list) if room_number_list.count(item) == 1]
     print(unique_room[0])
 
-    # not mine
+    # not mine, pass the time
     k, arr = int(input()), list(map(int, input().split()))
     myset = set(arr)
     print(((sum(myset) * k) - (sum(arr))) // (k - 1))
