24077c3cac9f0e6d0fef17b25a80af6f0a9e5e8e
Dmytro Verbytskyi <dmytro.qa.engineer@gmail.com>
2020-11-22 18:24:40 +0200

    Designer Door Mat

--- Python Practice/Strings/Designer Door Mat.py
+++ Python Practice/Strings/Designer Door Mat.py
@@ -1,1 +1,14 @@
-""" problem statement - https://www.hackerrank.com/challenges/text-wrap/problem """
+""" problem statement - https://www.hackerrank.com/challenges/designer-door-mat/problem """
+
+
+def print_design_pattern(width, height):
+    for top in range(1, width, 2):
+        print((top * ".|.").center(height, "-"))
+    print("WELCOME".center(height, "-"))
+    for bottom in range(width-2, 0, -2):
+        print((bottom * ".|.").center(height, "-"))
+
+
+if __name__ == '__main__':
+    N, M = map(int, input().split())
+    print_design_pattern(N, M)
